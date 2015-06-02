be4b6dc7ee9be844ac3ea09ad133fd4632132724
JLev <josh@sensorswitch.com>
2015-06-02 11:38:45 -0400

    Refactored some logics into functionssssss

--- romans.rb
+++ romans.rb
@@ -1,69 +1,50 @@
+# require 'pry'
+
 input = ARGV[0].to_i
 output = ""
+numerals = Hash.new
 numerals = { "M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1}
 
 output += "M" * (input / 1000)
 
-remainder = input % 1000
+$remainder = input % 1000
 
-if remainder >= 900
-  output += "CM"
-  remainder -= 900
+def getWholeNumerals(nextNumeral, numerals)
+  ret = ""
+  if $remainder >= numerals[nextNumeral]
+    ret += (nextNumeral * ($remainder / numerals[nextNumeral])).to_s
+    $remainder = $remainder % numerals[nextNumeral]
+    # puts "Remainder is " + $remainder.to_s
+  end
+  return ret
 end
 
-if remainder >= 500
-  output += "D"
-  remainder -= 500
+def getPrefixedNumerals(nextNumeral, prefix, numerals)
+  ret = "" 
+  if $remainder >= (numerals[nextNumeral] - numerals[prefix])
+    ret += (prefix + nextNumeral).to_s
+    $remainder -= (numerals[prefix] + numerals[nextNumeral])
+    # puts "Remainder is " + $remainder.to_s
+  end
+  return ret
 end
 
-if remainder >= 400
-  output += "CD"
-  remainder -= 400
+while $remainder > 0
+  output += getPrefixedNumerals('M', 'C', numerals)
+  output += getWholeNumerals('D', numerals)
+  output += getPrefixedNumerals('D', 'C', numerals)
+  output += getWholeNumerals('C', numerals)
+  output += getPrefixedNumerals('C', 'X', numerals)
+  output += getWholeNumerals('L', numerals)
+  output += getPrefixedNumerals('L', 'X', numerals)
+  output += getWholeNumerals('X', numerals)
+  output += getPrefixedNumerals('X', 'I', numerals)
+  output += getWholeNumerals('V', numerals)
+  output += getPrefixedNumerals('V', 'I', numerals)
+  output += getWholeNumerals('I', numerals)
 end
 
-if remainder >= 100
-  output += "C" * (remainder / 100)
-  remainder = remainder % 100
-end
-
-if remainder >= 90
-  output += "XC"
-  remainder -= 90
-end
-
-if remainder >= 50
-  output += "L"
-  remainder -= 50
-end
-
-if remainder >= 40
-  output += "XL"
-  remainder -= 40
-end
-
-if remainder >= 10
-  output += "X" * (remainder / 10)
-  remainder = remainder % 10
-end
-
-if remainder >= 9
-  output += "IX"
-  remainder -= 9
-end
-
-if remainder >= 5
-  output += "V"
-  remainder -= 5
-end
-
-if remainder >= 4
-  output += "IV"
-  remainder -= 4
-end
-
-if remainder >= 1
-  output += "I" * remainder
-end
+# binding.pry
 
 puts output
 
